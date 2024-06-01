d23cdfebbc0f7b9e3af722b9d939cc0d4cec926b
Lumos2050 <94121245+Lumos2050@users.noreply.github.com>
2024-06-01 20:00:14 +0800

    Add files via upload

---
+++ Test4/Diff_process.py
@@ -0,0 +1,316 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+import random
+import cv2
+import os
+from tifffile import imread
+def setup_seed(seed):
+     torch.manual_seed(seed)
+     torch.cuda.manual_seed_all(seed)
+     np.random.seed(seed)
+     random.seed(seed)
+     torch.backends.cudnn.deterministic = True
+def kappa(confusion_matrix, k):
+    dataMat = np.mat(confusion_matrix)
+    P0 = 0.0
+    for i in range(k):
+        P0 += dataMat[i, i]*1.0
+    xsum = np.sum(dataMat, axis=1)
+    ysum = np.sum(dataMat, axis=0)
+    Pe  = float(ysum*xsum)/np.sum(dataMat)**2
+    OA = float(P0/np.sum(dataMat)*1.0)
+    cohens_coefficient = float((OA-Pe)/(1-Pe))
+    return cohens_coefficient
+# 设置随机数种子
+
+
+# 1.定义网络超参数
+setup_seed(3407)
+BATCH_SIZE = 50# 每次喂给的数据量
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'   # 是否用GPU环视cpu训练
+
+if_cuda = torch.cuda.is_available()
+#print("if_cuda=",if_cuda)
+gpu_count = torch.cuda.device_count()
+#print("gpu_count=",gpu_count)
+
+#ms4_np = imread('/root/autodl-tmp/MyCode/dataset/ms4.tif').astype("float32")
+ms4_np = imread('./dataset/ms4.tif').astype("float32")
+ms4_np = cv2.resize(ms4_np, dsize=None, fx=4, fy=4, interpolation=cv2.INTER_LINEAR)
+#print(ms4_np.shape, ms4_np.dtype, np.min(ms4_np), np.max(ms4_np))
+
+#pan_np = imread('/root/autodl-tmp/MyCode/dataset/pan.tif').astype("float32")
+pan_np = imread('./dataset/pan.tif').astype("float32")
+#print(pan_np.shape, pan_np.dtype, np.min(pan_np), np.max(pan_np))
+
+#label_np = np.load("/root/autodl-tmp/MyCode/dataset/label.npy")
+label_np = np.load("./dataset/label.npy")
+#print('label数组形状：', np.shape(label_np))
+
+#train_label_np = np.load("/root/autodl-tmp/MyCode/dataset/train.npy")
+train_label_np = np.load("./dataset/train.npy")
+#print('train_label数组形状：', np.shape(train_label_np))
+
+#test_label_np = np.load("/root/autodl-tmp/MyCode/dataset/test.npy")
+test_label_np = np.load("./dataset/test.npy")
+#print('test_label数组形状：', np.shape(test_label_np))
+
+# ms4与pan图补零  (给图片加边框）
+Ms4_patch_size = 64 # ms4截块的边长
+Interpolation = cv2.BORDER_REFLECT_101
+# cv2.BORDER_REPLICATE： 进行复制的补零操作;
+# cv2.BORDER_REFLECT:  进行翻转的补零操作:gfedcba|abcdefgh|hgfedcb;
+# cv2.BORDER_REFLECT_101： 进行翻转的补零操作:gfedcb|abcdefgh|gfedcb;
+# cv2.BORDER_WRAP: 进行上下边缘调换的外包复制操作:bcdefgh|abcdefgh|abcdefg;
+
+top_size, bottom_size, left_size, right_size = (int(Ms4_patch_size / 2 - 1), int(Ms4_patch_size / 2),
+                                                int(Ms4_patch_size / 2 - 1), int(Ms4_patch_size / 2))
+ms4_np = cv2.copyMakeBorder(ms4_np, top_size, bottom_size, left_size, right_size, Interpolation)
+#print('补零后的ms4_np图的形状：', np.shape(ms4_np))
+
+Pan_patch_size = Ms4_patch_size   # pan截块的边长
+top_size, bottom_size, left_size, right_size = (int(Pan_patch_size / 2 - 1), int(Pan_patch_size / 2),
+                                                int(Pan_patch_size / 2 - 1), int(Pan_patch_size / 2))
+pan_np = cv2.copyMakeBorder(pan_np , top_size, bottom_size, left_size, right_size, Interpolation)
+#print('补零后的pan_np 图的形状：', np.shape(pan_np ))#长和宽都比原先多了31，即Ms4_patch_size-1
+
+
+label_np = label_np - 1  # 标签中0类标签是未标注的像素，通过减一后将类别归到0-N，而未标注类标签变为255
+label_element, element_count = np.unique(label_np, return_counts=True)  # 返回类别标签与各个类别所占的数量
+#print('类标：', label_element)
+#print('各类样本数：', element_count)
+Categories_Number = len(label_element) - 1  # 数据的类别数
+#print('标注的类别数：', Categories_Number)
+label_row, label_column = np.shape(label_np)  # 获取标签图的行、列
+
+
+train_label_np = train_label_np - 1  # 标签中0类标签是未标注的像素，通过减一后将类别归到0-N，而未标注类标签变为255
+train_label_element, train_element_count = np.unique(train_label_np, return_counts=True)  # 返回类别标签与各个类别所占的数量
+#print('类标：', train_label_element)
+#print('各类样本数：', train_element_count)
+train_Categories_Number = len(train_label_element) - 1  # 数据的类别数
+#print('标注的类别数：', train_Categories_Number)
+train_label_row, train_label_column = np.shape(train_label_np)  # 获取标签图的行、列
+
+
+test_label_np = test_label_np - 1  # 标签中0类标签是未标注的像素，通过减一后将类别归到0-N，而未标注类标签变为255
+test_label_element, test_element_count = np.unique(test_label_np, return_counts=True)  # 返回类别标签与各个类别所占的数量
+#print('类标：', test_label_element)
+#print('各类样本数：', test_element_count)
+test_Categories_Number = len(test_label_element) - 1  # 数据的类别数
+#print('标注的类别数：', test_Categories_Number)
+test_label_row, test_label_column = np.shape(test_label_np)  # 获取标签图的行、列
+
+
+ground_xy_train = np.array([[]] * Categories_Number).tolist()   # [[],[],[],[],[],[],[]]  7个类别
+ground_xy_test = np.array([[]] * Categories_Number).tolist()   # [[],[],[],[],[],[],[]]  7个类别
+
+ground_xy_trainallData = np.arange(train_label_row * train_label_column * 2).reshape(train_label_row * train_label_column, 2)  # [800*830, 2] 二维数组
+ground_xy_testallData = np.arange(test_label_row * test_label_column * 2).reshape(test_label_row * test_label_column, 2)  # [800*830, 2] 二维数组
+
+count1 = 0
+count2 = 0
+print("正在图像处理请稍候......")
+for row in range(train_label_row):  
+    for column in range(train_label_column):
+        #所有样本点的坐标[800*830, 2]
+        ground_xy_trainallData[count1] = [row, column]
+        count1 = count1 + 1
+        if train_label_np[row][column] != 255:
+            #在对应类别的列表里添加对应类别的坐标
+            ground_xy_train[int(train_label_np[row][column])].append([row, column])    
+            
+for row in range(test_label_row):  
+    for column in range(test_label_column):
+        ground_xy_testallData[count2] = [row, column]
+        count2 = count2 + 1
+        if test_label_np[row][column] != 255:
+            ground_xy_test[int(test_label_np[row][column])].append([row, column])  
+print("正在图像处理请稍候......")
+###train各类别内部打乱
+for categories in range(Categories_Number):
+    ########
+    ground_xy_train[categories] = np.array(ground_xy_train[categories])
+    shuffle_array = np.arange(0, len(ground_xy_train[categories]), 1)
+    np.random.shuffle(shuffle_array)
+
+    ground_xy_train[categories] = ground_xy_train[categories][shuffle_array]
+
+###test各类别内部打乱
+for categories in range(Categories_Number):
+    ########
+    ground_xy_test[categories] = np.array(ground_xy_test[categories])
+    shuffle_array = np.arange(0, len(ground_xy_test[categories]), 1)
+    np.random.shuffle(shuffle_array)
+
+    ground_xy_test[categories] = ground_xy_test[categories][shuffle_array]
+
+    
+ground_xy_ltrain = []
+ground_xy_ltest = []
+label_train = []
+label_test = []
+
+
+for categories in range(Categories_Number):
+    categories_number_train = len(ground_xy_train[categories])
+    for i in range(categories_number_train):
+        ground_xy_ltrain.append(ground_xy_train[categories][i])
+    label_train = label_train + [categories for x in range(int(categories_number_train))]
+
+
+for categories in range(Categories_Number):
+    categories_number_test = len(ground_xy_test[categories])
+    for i in range(int(categories_number_test * 0.05)):####################可在此调节test数据集大小#################################
+        ground_xy_ltest.append(ground_xy_test[categories][i])
+    label_test = label_test + [categories for x in range(int(categories_number_test * 0.05))]####################可在此调节test数据集大小#################################
+
+label_train = np.array(label_train)
+label_test = np.array(label_test)
+ground_xy_ltrain = np.array(ground_xy_ltrain)
+ground_xy_ltest = np.array(ground_xy_ltest)
+
+#train整体打乱
+shuffle_array = np.arange(0, len(label_train), 1)
+np.random.shuffle(shuffle_array)
+label_train = label_train[shuffle_array]
+ground_xy_ltrain = ground_xy_ltrain[shuffle_array]
+#test整体打乱
+shuffle_array = np.arange(0, len(label_test), 1)
+np.random.shuffle(shuffle_array)
+label_test = label_test[shuffle_array]
+ground_xy_ltest = ground_xy_ltest[shuffle_array]
+
+label_train = torch.from_numpy(label_train).type(torch.LongTensor)
+label_test = torch.from_numpy(label_test).type(torch.LongTensor)
+ground_xy_ltrain = torch.from_numpy(ground_xy_ltrain).type(torch.LongTensor)
+ground_xy_ltest = torch.from_numpy(ground_xy_ltest).type(torch.LongTensor)
+
+#图像处理
+def to_tensor(image):
+    max_i = np.max(image)
+    min_i = np.min(image)
+    image = (image - min_i) / (max_i - min_i)
+    return image
+
+
+ms4 = to_tensor(ms4_np)
+pan = to_tensor(pan_np)
+alpha = np.array([0.18, 0.27, 0.11, 0.59]).astype(np.float32)
+ms4 = np.transpose(ms4, (2, 0, 1))
+alpha = alpha.reshape(-1, 1, 1)
+MP = alpha * ms4
+I_m = np.sum(MP, axis=0, keepdims=False)#通过设置 keepdims=True，保持了结果的维度为 (3200, 3320, 1)，而不会直接变成 (3200, 3320)
+PT = pan - I_m
+MT = (1 - alpha) * ms4
+#print(MP.shape, PT.shape, MP.shape)
+label_fu,label_pan,label_ms=[],[],[]
+
+def image_gradient(img):
+    H, W = img.shape
+    gx = np.pad(np.diff(img, axis=0), ((0,1),(0,0)), 'constant')
+    gy = np.pad(np.diff(img, axis=1), ((0,0),(0,1)), 'constant')
+    gradient = abs(gx) + abs(gy)
+    return gradient
+def edge_dect(img):
+    nam=1e-9
+    apx=1e-10
+    return np.exp( -nam / ( (image_gradient(img)**4)+apx ) )
+
+#print(MP.shape, PT.shape, MP.shape)
+print("正在图像处理请稍候......")
+# edge detection operator
+W_mi = [ edge_dect(ms4[i]) for i in range(4) ]
+W_pi = edge_dect(pan)
+m_sum = (ms4[1] + ms4[2] + ms4[3] + ms4[0]) /4
+ 
+gamma = 0.3 #0 < gamma < 0.5
+W_m  = [ms4[i] / m_sum*( W_mi[i] ) for i in range(4)]
+W_m_av = (ms4[1] + ms4[2] + ms4[3] + ms4[0]) / m_sum /4
+W_m_ava =( W_m[1] + W_m[2] + W_m[3] + W_m[0] ) / 4
+W_p = gamma * W_m_ava + (1 - gamma) * W_m_av * W_pi
+
+
+
+# fusion
+PT_1 = PT * W_p
+MT_1 = np.zeros_like(MT)
+MP_1 = np.zeros_like(MP)
+for i in range(4):
+    MT_1[i] = MT[i] * W_m[i]
+    MP_1[i] = MP[i] * W_m[i]
+
+PT_1 = to_tensor(PT_1)
+MT_1 = to_tensor(MT_1)
+MP_1 = to_tensor(MP_1)
+
+class MyData(Dataset):
+    def __init__(self, MT, MP, PT, Label, xy, cut_size):
+        self.train_data1 = MT
+        self.train_data2 = MP
+        self.train_data3 = PT
+        self.train_labels = Label
+        self.gt_xy = xy
+        self.cut_ms_size = cut_size
+        self.cut_pan_size = cut_size
+    def __getitem__(self, index):
+        x_ms, y_ms = self.gt_xy[index]
+        x_pan = int( x_ms)      
+        y_pan = int( y_ms)
+        image_MT = self.train_data1[:, x_ms:x_ms + self.cut_ms_size,
+                   y_ms:y_ms + self.cut_ms_size]
+        image_MP = self.train_data2[:, x_ms:x_ms + self.cut_ms_size,
+                   y_ms:y_ms + self.cut_ms_size]        
+        image_PT = self.train_data3[x_pan:x_pan + self.cut_pan_size,
+                    y_pan:y_pan + self.cut_pan_size]
+        
+
+        image_PT = np.expand_dims(image_PT, axis=0)
+        locate_xy = self.gt_xy[index]
+        target = self.train_labels[index]
+        # 打印数据形状
+        #print("Shape of MP image:", image_MT.shape)
+        #print("Shape of MP image:", image_MP.shape)
+        #print("Shape of PT image:", image_PT.shape)
+        #print("Target label:", target)
+        #print("Location:", locate_xy)
+        return image_MT, image_MP, image_PT, target, locate_xy
+
+    def __len__(self):
+        return len(self.gt_xy)
+#只返回图像、位置   
+class MyData1(Dataset):
+    def __init__(self, MT, MP, PT, xy, cut_size):
+        self.train_data1 = MT
+        self.train_data2 = MP
+        self.train_data3 = PT
+        self.gt_xy = xy
+        self.cut_ms_size = cut_size
+        self.cut_pan_size = cut_size
+
+    def __getitem__(self, index):
+        x_ms, y_ms = self.gt_xy[index]
+        x_pan = int(x_ms)  
+        y_pan = int(y_ms)
+
+        image_MT = self.train_data1[:, x_ms:x_ms + self.cut_ms_size,
+                   y_ms:y_ms + self.cut_ms_size]
+        image_MP = self.train_data2[:, x_ms:x_ms + self.cut_ms_size,
+                   y_ms:y_ms + self.cut_ms_size]        
+        image_PT = self.train_data3[x_pan:x_pan + self.cut_pan_size,
+                    y_pan:y_pan + self.cut_pan_size]        
+        image_PT = np.expand_dims(image_PT, axis=0)
+
+        locate_xy = self.gt_xy[index]
+
+        return image_MT, image_MP, image_PT, locate_xy
+
+    def __len__(self):
+        return len(self.gt_xy)
+train_data = MyData(MT, MP, PT, label_train,ground_xy_ltrain, Ms4_patch_size)
+test_data = MyData(MT, MP, PT, label_test ,ground_xy_ltest, Ms4_patch_size)########
+train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
+test_loader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+
---
+++ Test4/edge_sv.py
@@ -0,0 +1,367 @@
+import torch
+import cv2
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+def normalize_images(images):
+    """
+    对多通道图像数据进行归一化操作。
+
+    参数：
+    - images: 形状为 (B, 4, H, W) 的图像数据，其中 B 是批量大小，H 和 W 分别是图像的高度和宽度。
+
+    返回：
+    - normalized_images: 归一化后的图像数据，形状与输入相同。
+    """
+    images_cpu = images#.detach().cpu().numpy() 
+    normalized_images = np.zeros_like(images_cpu, dtype=np.float32)
+
+    for i in range(images_cpu.shape[0]):  # 对每个批次的图像进行处理
+        for j in range(images_cpu.shape[1]):
+            channel_data = images_cpu[i, j]  # 获取当前通道的数据
+            mean = np.mean(channel_data)  # 计算当前通道的均值
+            std = np.std(channel_data)  # 计算当前通道的标准差
+            normalized_channel_data = (channel_data - mean) / (std + 1e-8)  # 应用归一化
+            min_value = np.min(normalized_channel_data)  # 计算归一化后数据的最小值
+            max_value = np.max(normalized_channel_data)  # 计算归一化后数据的最大值
+            scaled_data = (normalized_channel_data - min_value) / (max_value - min_value)  # 缩放数据到 [0, 1] 范围内
+            normalized_images[i, j] = scaled_data # 更新归一化后的图像数据
+    return normalized_images
+
+
+
+def canny_weight(msf_init, pan_init, seg_edge, low = 50, high = 150):
+    """
+    input: msf_init(B, 1, H, W), pan_init(B, 1, H, W), seg(B, 4, H, W)
+    output: weighted_image(B, 1, H, W), mse_msf, mse_pan
+    """
+    msf_init = msf_init.detach().cpu().numpy()
+    pan_init = pan_init.detach().cpu().numpy()
+    seg_edge = seg_edge.detach().cpu().numpy()
+
+
+    #--------------------归一化-----------------
+    normalize_msf_init = normalize_images(msf_init)
+    normalize_pan_init = normalize_images(pan_init)    
+    normalize_seg_edge = normalize_images(seg_edge)
+
+
+    #---------------canny-----------------------
+    msf_init_edge = np.zeros_like(msf_init)
+    pan_init_edge = np.zeros_like(pan_init)
+    seg_edge_edge = np.zeros_like(seg_edge)
+    for i in range(normalize_msf_init.shape[0]):  # 对每张图像应用Canny算法
+        single_image0 = (normalize_msf_init[i, 0]*255).astype(np.uint8)  # 将图像数据转换为8位无符号整数类型
+        msf_init_edge[i, 0] = cv2.Canny(single_image0, low, high)
+    for i in range(normalize_pan_init.shape[0]):  # 对每张图像应用Canny算法
+        single_image1 = (normalize_pan_init[i, 0]*255).astype(np.uint8)  # 将图像数据转换为8位无符号整数类型
+        pan_init_edge[i, 0] = cv2.Canny(single_image1, low, high)
+
+    for i in range(normalize_seg_edge.shape[0]):  # 对每个批次的图像进行处理
+        for j in range(normalize_seg_edge.shape[1]):  # 对每张图像的每个通道进行处理
+            single_image2 = (normalize_seg_edge[i, j]*255).astype(np.uint8)  # 将图像数据转换为8位无符号整数类型
+            seg_edge_edge[i, j] = cv2.Canny(single_image2, low, high)
+
+    #print("msf_init_edge的形状", msf_init_edge.shape)
+    #print("seg_edge_edge的形状", seg_edge_edge.shape)
+    #print("pan_init_edge的形状", pan_init_edge.shape)
+    msf_init_edge = msf_init_edge /255.0
+    pan_init_edge = pan_init_edge / 255.0
+    seg_edge_edge = seg_edge_edge/255.0
+
+    batch_size = seg_edge_edge.shape[0]
+    mse_msf = np.zeros(batch_size)
+    mse_pan = np.zeros(batch_size)
+    for j in range (batch_size):
+        for i in range(seg_edge_edge.shape[1]):  # 对第二张图像的每个通道进行遍历
+            mse_msf[j] =np.mean((msf_init_edge[j] - seg_edge_edge[j, i:i+1])**2)  # 计算当前通道的均方误差
+            mse_pan[j] =np.mean((pan_init_edge[j] - seg_edge_edge[j, i:i+1])**2)  # 计算当前通道的均方误差
+
+
+    # 使用 tanh 函数计算权重
+    weight_msf = np.tanh(mse_msf)
+    weight_pan = np.tanh(mse_pan)
+    
+    # 对权重进行归一化
+
+    # 计算权重和
+    weight_sum = weight_msf + weight_pan
+
+    # 归一化权重
+    for i in range(batch_size):
+        if weight_sum[i].item() != 0:
+            weight_msf[i] /= weight_sum[i].item()
+            weight_pan[i] /= weight_sum[i].item()
+        else:
+            # 如果权重和为零，将权重设置为均匀分布
+            print("除以零，错误")
+    
+    weight_msf_np = np.expand_dims(np.expand_dims(np.expand_dims(weight_msf, axis=-1), axis=-1), axis=-1)
+    weight_pan_np = np.expand_dims(np.expand_dims(np.expand_dims(weight_pan, axis=-1), axis=-1), axis=-1)
+    # 按权重相加
+    weighted_image = msf_init * weight_msf_np + pan_init * weight_pan_np
+    #print("mse_msf的形状", mse_msf.shape) 
+    #print("mse_pan的形状", mse_pan.shape)
+    #print("weighted_image的形状",  weighted_image.shape)
+    return weighted_image, normalize_seg_edge, mse_msf, mse_pan
+
+#mse_msf, mse_pan要加到loss里
+
+
+
+def entropy(image):
+    # 计算信息熵
+    hist = torch.histc(image, bins=256, min=0, max=1)
+    hist /= torch.sum(hist)
+    entropy = -torch.sum(hist * torch.log2(hist + 1e-12))  # 添加一个小值避免 log(0) 的情况
+    return entropy
+
+def weighted_sum(msf_init, pan_init):
+    """
+    input: msf_init(B, 1, H, W), pan_init(B, 1, H, W)
+    output: weighted_image(B, 1, H, W)
+    """
+    # 计算每张图像的熵值
+    entropy_msf = entropy(msf_init)
+    entropy_pan = entropy(pan_init)
+    
+    # 使用 tanh 函数计算权重
+    weight_msf = torch.tanh(entropy_msf)
+    weight_pan = torch.tanh(entropy_pan)
+    
+    # 对权重进行归一化
+    weight_sum = weight_msf + weight_pan
+    weight_msf /= weight_sum if weight_sum.item() != 0 else 1
+    weight_pan /= weight_sum if weight_sum.item() != 0 else 1
+    
+    # 按权重相加
+    weighted_image = msf_init * weight_msf + pan_init * weight_pan
+    
+    return weighted_image
+
+# 示例用法
+# 假设 msf_init 和 pan_init 分别是两个形状为 (B, 1, H, W) 的张量
+# weighted_image = weighted_sum(msf_init, pan_init)
+
+class ResBlk(nn.Module):
+    """
+    resnet block
+    """
+    def __init__(self, ch_in, ch_out, stride=1):
+        """
+        :param ch_in:
+        :param ch_out:
+        """
+        super(ResBlk, self).__init__()
+        self.conv1 = nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(ch_out)
+        self.conv2 = nn.Conv2d(ch_out, ch_out, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(ch_out)
+
+        self.extra = nn.Sequential()
+        if ch_out != ch_in:
+            # [b, ch_in, h, w] => [b, ch_out, h , w]
+            self.extra = nn.Sequential(
+                nn.Conv2d(ch_in, ch_out, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(ch_out)
+            )
+
+    def forward(self, x):
+        """
+        :param x: [b, ch, h, w]
+        :return:
+        """
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        # short cut
+        # element_wise add:[b, ch_in, h, w] with [b, ch_out, h ,w]
+        out = self.extra(x) + out
+
+        return out
+
+class ResNet18(nn.Module):
+
+    def __init__(self):
+        super(ResNet18, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(4, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64)
+        )
+
+        #followed 4 blocks
+        #[b, 64, h, w] => [b, 64, h , w]
+        self.blk1_1 = ResBlk(64, 128, stride=1)
+
+        #[b, 64, h, w] =>[b , 128, h ,w]
+        #self.blk2_1 = ResBlk(64, 128, stride=1)
+
+        #[b, 128, h, w] =>[b , 256, h ,w]
+        self.blk3_1 = ResBlk(128, 256, stride=1)
+
+        # [b, 256, h, w] =>[b , 512, h ,w]
+        self.blk4_1 = ResBlk(256, 512, stride=1)
+
+        self.outlayer1 = nn.Linear(512, 256)
+        self.outlayer2 = nn.Linear(256, 128)
+        self.outlayer3 = nn.Linear(128, 11)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        """
+        :param x:#(B, 4, H, W)
+        :return:
+        """
+        #print("input x", x.shape)
+        x = F.relu(self.conv1(x))
+        #[b, 64, h, w] => [b, 512, h , w]
+        x = self.blk1_1(x)
+        #x = self.blk2_1(x)
+        x = self.blk3_1(x)
+        x = self.blk4_1(x)
+        x = F.adaptive_avg_pool2d(x, [1, 1])
+
+
+        #print('x', x.shape)
+
+        #print(x.size())
+        x = x.view(x.size()[0],  -1)
+        #print('ssss', x.shape)
+        x = F.relu(self.outlayer1(x))
+        x = F.relu(self.outlayer2(x))
+        x = F.relu(self.outlayer3(x))
+        x = F.softmax(x, dim=1)
+        #print('ssssssssss',x.shape)
+        return x
+    
+class SqueezeBodyEdge(nn.Module):
+    """
+    input: x:torch.Size([1, 4, 32, 32]) inplane = 4
+    output: seg_flow_warp(就是body), seg_edge, 都是torch.Size([1, 4, 32, 32]) 
+    """
+
+
+    def __init__(self, inplane):
+        """
+        implementation of body generation part
+        :param inplane:
+        :param norm_layer:
+        """
+        super(SqueezeBodyEdge, self).__init__()
+        #四倍降采样，还用的是深度可分离卷积
+        self.down = nn.Sequential(
+            nn.Conv2d(inplane, inplane, kernel_size=3, groups=inplane, stride=2),
+            # norm_layer(inplane),
+            nn.BatchNorm2d(inplane),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inplane, inplane, kernel_size=3, groups=inplane, stride=2),
+            # norm_layer(inplane),
+            nn.BatchNorm2d(inplane),
+            nn.ReLU(inplace=True)
+        )
+        self.flow_make = nn.Sequential(
+            nn.Conv2d(inplane * 2, 2, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(2),
+            nn.ReLU(inplace=True))
+        self.upsample2x_op = nn.UpsamplingBilinear2d(scale_factor=2)
+        self.conv_5to4 = nn.Sequential(
+            nn.Conv2d(inplane + 1, inplane, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(inplane),
+            nn.ReLU(inplace=True))
+        self.fc = ResNet18()
+    def flow_warp(self, input, flow, size):
+        out_h, out_w = size
+        n, c, h, w = input.size()
+
+        norm = torch.tensor([[[[out_w, out_h]]]]).type_as(input).to(input.device)
+        # new
+        h_grid = torch.linspace(-1.0, 1.0, out_h).view(-1, 1).repeat(1, out_w)
+        w_gird = torch.linspace(-1.0, 1.0, out_w).repeat(out_h, 1)
+        grid = torch.cat((w_gird.unsqueeze(2), h_grid.unsqueeze(2)), 2)
+
+        grid = grid.repeat(n, 1, 1, 1).type_as(input).to(input.device)
+        grid = grid + flow.permute(0, 2, 3, 1) / norm
+
+        output = F.grid_sample(input, grid)
+        return output
+    
+    def forward(self, x, msf_init, pan_init):
+        """
+        input: x:torch.Size([1, 4, H/2, W/2])
+        x(B, 4, H/2, W/2), msf_init, pan_init(B, 1, H, W)
+        """
+        #print("边缘监督中的释放显存1")
+        torch.cuda.empty_cache()
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        x = x.to(device)
+        x = x.detach().cpu().numpy()
+        x = normalize_images(x)
+        x = torch.from_numpy(x).cuda()
+
+
+        msf_init = msf_init.to(device)
+        pan_init = pan_init.to(device)
+
+#init的归一化在canny函数里面
+
+        size = x.size()[2:]#H和W
+        seg_down = self.down(x)
+        seg_down = F.upsample(seg_down, size=size, mode="bilinear", align_corners=True)
+        #concat成8通道，然后变成两通道
+        flow = self.flow_make(torch.cat([x, seg_down], dim=1))#(B, 2, H/2, W/2)
+
+        seg_flow_warp = self.flow_warp(x, flow, size)#(B, 4, H/2, W/2)
+        seg_edge = x - seg_flow_warp
+
+
+        #print(seg_edge.shape, seg_flow_warp.shape)
+        #torch.Size([1, 4, 64, 64]) torch.Size([1, 4, 64, 64])
+
+        #-----------------Enhancement--------------------------------------
+        seg_edge =  self.upsample2x_op(seg_edge)#(B, 4, H/2, W/2)->(B, 4, H, W)
+
+        #print("1seg_edge", seg_edge.shape)
+
+        seg_flow_warp = self.upsample2x_op(seg_flow_warp)#(B, 4, H/2, W/2)->(B, 4, H, W)
+        seg_flow_warp = seg_flow_warp.detach().cpu().numpy()
+        seg_flow_warp = normalize_images(seg_flow_warp)
+        seg_flow_warp = torch.from_numpy(seg_flow_warp).cuda()
+
+        weighted_image, seg_edge, mse_msf, mse_pan = canny_weight(msf_init, pan_init, seg_edge, low = 50, high = 150)#包含归一化(B, 1, H, W)
+
+        #print("2seg_edge", seg_edge.shape, weighted_image.shape)
+        weighted_image = torch.from_numpy(weighted_image).cuda()
+        seg_edge = torch.from_numpy(seg_edge).cuda()
+        weighted_image = weighted_image.float()  # 将输入数据转换为 torch.cuda.FloatTensor 类型
+        seg_edge = seg_edge.float() 
+
+
+        #print("seg_edge,weighted_iamge的数据类型", seg_edge.dtype, weighted_image.dtype)
+        fine_edge = self.conv_5to4(torch.cat([seg_edge, weighted_image], dim = 1))#(B, 4, H, W)
+        final = seg_flow_warp + fine_edge#(B, 4, H, W)
+        #print("边缘监督中的释放显存2")
+        torch.cuda.empty_cache()
+        cls_prd = self.fc(final)
+        return seg_flow_warp, seg_edge, mse_msf, mse_pan, cls_prd
+
+"""
+if __name__ == '__main__':
+    height = 64
+    width = 64
+    model = SqueezeBodyEdge(
+        inplane = 4
+    ).cuda().eval()
+
+    print(height, width)
+
+    x = torch.randn((5, 4, int(height/2), int(width/2)),dtype=torch.float32).cuda()
+    msf_init = torch.randn((5, 1, height, width),dtype=torch.float32).cuda()
+    pan_init = torch.randn((5, 1, height, width),dtype=torch.float32).cuda()
+
+    _, _, mse_msf, mse_pan, cls_prd= model(x, msf_init, pan_init)
+
+    print(mse_msf, mse_pan, cls_prd.shape)
+    #698.5107421875 1016.015625 torch.Size([1, 7])
+    #[174.62768555 174.62768555 142.87719727 174.62768555 142.87719727] [174.62768555 174.62768555 142.87719727 174.62768555 142.87719727] torch.Size([5, 11])
+"""
